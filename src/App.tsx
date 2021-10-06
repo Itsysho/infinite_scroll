880ea473f93ed0bca76261a41f0f456693b12f1a
Ho, Yi-Shin <itisysho@gmail.com>
2021-10-06 23:03:33 +0800

    feat: add CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import PostList from './containers/PostList'
 import { routerPath } from './common/appConfig'
+import { CssBaseline, theme } from './common/styled'
 
 function App() {
   return (
-    <main data-testid='app'>
-      <Router>
-        <Route path={routerPath.root}>
-          <PostList />
-        </Route>
-        <Redirect to={routerPath.root} />
-      </Router>
-    </main>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main data-testid='app'>
+        <Router>
+          <Route path={routerPath.root}>
+            <PostList />
+          </Route>
+          <Redirect to={routerPath.root} />
+        </Router>
+      </main>
+    </ThemeProvider>
   )
 }
 
